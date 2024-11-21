392b8aeaed590c5bb64d7cc44875c89701ed18de
themanhhhh <131895922+themanhhhh@users.noreply.github.com>
2024-11-21 07:03:19 +0700

    Update author.js

--- pages/author.js
+++ pages/author.js
@@ -22,26 +22,32 @@ const author = () => {
       {
         background: images.creatorbackground10,
         user : images.user1,
+        seller: "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     },
     {
         background: images.creatorbackground11,
         user: images.user10,
+        seller: "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     },
     {
         background: images.creatorbackground3,
         user : images.user3,
+        seller: "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     },
     {
         background: images.creatorbackground7,
         user : images.user7,
+        seller: "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     },
     {
         background: images.creatorbackground9,
         user : images.user9,
+        seller: "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     },
     {
         background: images.creatorbackground8,
         user : images.user8,
+        seller: "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     },
     ]
     const [collectiables, setCollectiables] =useState(true);
@@ -97,6 +103,7 @@ const author = () => {
         <div className={Style.author_box}>
           {
             popularArray.map((el,i)=>(
+              
               <FollowerTabCard key={i+1} i={i} el={el}/>
             ))
           }
